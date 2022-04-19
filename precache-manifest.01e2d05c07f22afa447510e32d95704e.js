94314f5fa4494d44a2f59e90637b26f74249cabd
johnprovazek <johnprovazek@gmail.com>
2022-04-19 15:13:18 -0700

    Updates

---
+++ precache-manifest.01e2d05c07f22afa447510e32d95704e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "67fbc9ab44430385bdfa",
+    "url": "/mlbstandings/static/css/main.09284cb9.chunk.css"
+  },
+  {
+    "revision": "67fbc9ab44430385bdfa",
+    "url": "/mlbstandings/static/js/main.67fbc9ab.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/mlbstandings/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "e495a233d13d271953c2",
+    "url": "/mlbstandings/static/js/2.e495a233.chunk.js"
+  },
+  {
+    "revision": "13042ab1807ab57b4610",
+    "url": "/mlbstandings/static/js/runtime~main.13042ab1.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/mlbstandings/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a688c94a892c1289f08583680dcf263a",
+    "url": "/mlbstandings/index.html"
+  }
+];
